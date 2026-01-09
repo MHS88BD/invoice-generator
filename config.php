c77dd58bbb8b7ff47cfb2b475b0a26e4e09043b7
S M MEJBA UL HAQUE <mslive@Ss-MacBook-Pro.local>
2026-01-09 23:14:40 +0600

    Ensure config update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
 
-// Database Configuration
+// Database Configuration (MySQL)
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'invoice-app');
 define('DB_USER', 'invoice-user');
